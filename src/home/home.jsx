341a9ff1b5c7baf48df348d98f7b1c5afb694f11
jsai164 <sai.j@narmtech.com>
2024-10-03 18:08:03 +0530

    samplequizquestions style

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -225,7 +225,7 @@ function Home() {
                             className={`
                               ${(x === q.answerIndex && attempted.isAttempted) ? 'correctAnswer' : ''}
                               ${(x === attempted.answeredIndex && x !== q.answerIndex) ? 'wrongAnswer' : ''}
-                              mr-2 font-normal w-[40px] rounded-[5px] p-[8px] border-[1px] border-solid border-[#D3D3D3]   flex justify-center text-center justify-items-center items-center text-[10px]`}>
+                              mr-2 font-normal w-[40px] rounded-[5px] p-[8px] border-[1px] border-solid border-[#D3D3D3]  bg-[#E8E9E8]  flex justify-center text-center justify-items-center items-center text-[10px]`}>
                             {String.fromCharCode(97 + x).toUpperCase()}
                           </div>
                           <div
@@ -235,7 +235,7 @@ function Home() {
                             className={`
                                 ${(x === q.answerIndex && attempted.isAttempted) ? 'correctAnswer' : ''}
                                 ${(x === attempted.answeredIndex && x !== q.answerIndex) ? 'wrongAnswer' : ''}
-                                w-[50%] bg-[#E8E9E8]  rounded-[5px] border-solid border-[#D3D3D3] border-[1.8px] p-[5px] text-[11px] text-[#000]`}
+                                w-[50%] bg-[#E8E9E8]  h-[37px] rounded-[5px] border-solid border-[#D3D3D3] border-[1.8px] p-[5px] text-[11px] text-[#000]`}
                           >
                             {option.answer_option_text}
                           </div>

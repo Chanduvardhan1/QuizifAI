f79eac2cce713cb21c4afb04989015d2671851fa
jsai164 <sai.j@narmtech.com>
2024-10-05 12:41:15 +0530

    leader board design styles done

--- src/sample/sampleQuiz.jsx
+++ src/sample/sampleQuiz.jsx
@@ -7,7 +7,7 @@ import rank2Icon from "../../src/assets/Images/images/quizresults/rank2.png";
 import rank3Icon from "../../src/assets/Images/images/quizresults/rank3.png";
 import rightIcon1 from "../../src/assets/Images/images/quizresults/righticon.png";
 import timeIcon from "../../src/assets/Images/images/quizresults/stopwatch1.png";
-import "./sampleQuiz.css";
+import "./sampleQuiz.scss";
 import { useNavigate } from 'react-router-dom';
 import quizifailogo from "../assets/Images/images/home/Quizifai3.png";
 import homeImage from "/images/oldimage.png";
@@ -17,6 +17,7 @@ import current from "../../src/assets/Images/images/quizresults/faq.png"
 import { useSelector } from "react-redux";
 import SampleAnswerBoard from '../sampleQizAnswers/sampleAnswerBoard';
 import showUserAnswers, { setDynamicStateFlags } from "../../src/home/slice.jsx";
+import PageHeader from '../common/pageHeader.jsx';
 
 // const showAnswers = false
 const Samplequiz = () => {
@@ -65,49 +66,43 @@ const Samplequiz = () => {
   return (
 
     <div >
-      <div className='flex float-right gap-4px'>
-        <button className={`w-16 h-[30px] text-[#555555] text-[13px] leading-7 font-bold flex items-center justify-center rounded-full hover:bg-[#EF512F] transition-transform transform hover:scale-110 mt-[9%] mr-[5%] $`}>
-          Home
-        </button>
 
-        <button className={`w-20 h-[30px] rounded-full font-Poppins bg-[#3B61C8] text-white text-[13px] leading-7 font-semibold flex items-center justify-center gap-1 hover:bg-[#EF512F] transition-transform transform hover:scale-110 mt-[9%] mr-[3%]  $`}>
-          Sign Up
-          {/* <img className='h-[17px]' src={signUpIcon} alt="Sign Up Icon" /> */}
-        </button>
-        <button className={`w-20 h-[30px] rounded-full font-Poppins bg-[#3B61C8] text-white text-[13px] leading-7 font-semibold flex items-center justify-center gap-1 hover:bg-[#EF512F] transition-transform transform hover:scale-110  ml-[2%] mt-[9%]  mr-[6%] $`}>
-          Login
-          {/* <img className="h-[17px]" src={logInIcon} alt="Login Icon" /> */}
-        </button>
-      </div>
-
+      <PageHeader />
       <div className='flexContainer'>
 
-        <div className='subDiv1'>
-          <span>Your Attempt Details</span>
-          <div className='subDiv'>
-            <h1 className='rank'> {rank} </h1>
+        <div className='contentWrapper'>
+          <div className='content-details'>
+            <div className='content-details-attempt'>
+              <span className='leftLine line'></span>
+              <label>Your Attempt Details</label>
+              <span className='rightLine line'></span>
+            </div>
+          </div>
+          <div className='content-rank-flower'>
+            <label className='rank'> {rank} </label>
             <img className='h-[108.62px] w[130.01px  ml-[43%]'
               src={rankimage}
               alt="Icon 1"
-
             />
-            <span> Your Rank </span>
-            <div className=''>
-              <div className='mr-[3%] mt-[1%]'>
+            <span> Your Rank</span>
+          </div>
+          <div className='content-result'>
+            <div className='content-result-left'>
+              <div className='mr-[3%] mt-[1%] content-result-left-box'>
                 <img className='h-[30px] w[30.px] block inline mt-23px '
                   src={dateIcon}
                   alt="Calendar Icon"
                 />
                 <span className='ml-[9px]'> {dateString}</span>
               </div>
-              <div className='mr-[5%] mt-[1%]'>
+              <div className='mr-[5%] mt-[1%] content-result-left-box'>
                 <img className='h-[30px] w[30.px] block inline  '
                   src={timeIcon}
                   alt="Calendar Icon"
                 />
                 <span className='ml-[9px]'>15 Minutes</span>
               </div>
-              <div className='mt-[1%]'>
+              <div className='mt-[1%] content-result-left-box'>
                 <img className='h-[30px] w[30.px] block inline  '
                   src={vector}
                   alt="Calendar Icon"
@@ -115,19 +110,19 @@ const Samplequiz = () => {
                 />
                 <span className='ml-[9px]'>{leaderBoard.correctAnswers} correct answer</span>
               </div>
-              <div className='ml-[6%] mt-[1%]'>
+              <div className='ml-[6%] mt-[1%] content-result-left-box'>
                 <img className='h-[30px] w[30.px] block inline '
                   src={current}
                   alt="Calendar Icon"
 
                 />
-                <span className='ml-[9px] '>Attempted {leaderBoard.attemptedQuestions} questions</span>
+                <span className='ml-[9px]'>Attempted {leaderBoard.attemptedQuestions} questions</span>
               </div>
-              <div>
-
-                {/* <div className='ml-[74%] border border-blue-500 rounded-md transform -translate-y-10'>You have scored {percentage}%</div> */}
-                <div className='  top-0  mt-[22%] ml-[43%] border border-blue-500 rounded-md  transform -translate-y-1/2'> {percentage}%</div>
-                <div className=' top-[55%] ml-[43%] border border-blue-500 rounded-md '>{Grade} ,{passed} Grade</div>
+            </div>
+            <div className='content-result-right'>
+              <div className='content-result-right-box'>
+                <div className='w-[200px] rounded-md  transform -translate-y-1/2 content-result-right-box-round'> {percentage}%</div>
+                <div className='w-[200px] rounded-md content-result-right-box-round grade'>{Grade} ,{passed} Grade</div>
               </div>
             </div>
           </div>

5115f4bea4c06189aee5065e4ed9e2d29f1553ea
chanduvardhan <141388337+Chanduvardhan1@users.noreply.github.com>
2024-12-20 19:10:05 +0530

    new code

--- src/free-profile/free-profile.jsx
+++ src/free-profile/free-profile.jsx
@@ -339,7 +339,7 @@ const FreeProfile = () => {
       otp: otp,
       user_role: rolename, // Example value, adjust as needed
       user_type: "Public",
-      user_org_id: orgId,
+      // user_org_id: orgId,
       gender: gender,
       display_name: " ",
       date_of_birth: dob,
@@ -1780,44 +1780,7 @@ const FreeProfile = () => {
         </div>
 
         </div>
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <form
-        onSubmit={handleLogin}
-        className="w-full max-w-sm bg-white p-6 rounded shadow-md"
-      >
-        <h2 className="text-xl font-semibold text-center mb-4">Login</h2>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Email
-          </label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full px-3 py-2 border rounded text-gray-700 focus:outline-none focus:shadow-outline"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Password
-          </label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-3 py-2 border rounded text-gray-700 focus:outline-none focus:shadow-outline"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-        >
-          Login
-        </button>
-      </form>
-    </div>
+      
       </div>
       {/* <LogoutBar /> */}
     </div>
